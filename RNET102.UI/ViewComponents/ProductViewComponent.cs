96924f185e0ab084339bcfd56449d590b19f70a7
Mutalib SH. Gozalov <mutalibshg@code.local>
2023-06-08 13:57:43 +0400

    Product partial view added

---
+++ RNET102.UI/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RNET102.UI.Data;
+using RNET102.UI.Models;
+
+namespace RNET102.UI.ViewComponents;
+
+public class ProductViewComponent : ViewComponent
+{
+    private RNET102DbContext _context;
+    public ProductViewComponent(RNET102DbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+	{
+		var product = _context.Products.ToList();
+		return View(model: product);
+	}
+}
